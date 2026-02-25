d87fe392d5f7f9faeedde1da12228257cbaa8c6f
Barrdo123 <duzimantonio123@gmail.com>
2026-02-25 17:36:50 -0300

    feat: delete-and-update

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -3,7 +3,7 @@ import Registration from "../models/register.js";
 const registerController = {
     async createRegister(req, res){
         try{
-            const {category, type, description, value, userId} = req.body
+            const {category, type, description, value} = req.body
 
             if (!category || !type || !description || !value){
                 return res.status(400).json({
@@ -11,19 +11,28 @@ const registerController = {
                 });
             }
 
-            if(value < 0){
+            if(value !== undefined && value < 0){
                 return res.status(400).json({
                     message: "The value cannot be negative."
                 });
+                }
+            
+            
+            
+            if (!["revenue","expense"].includes(type)){
+                return res.status(400).json({
+                    message: "Check if the type is revenue or expense."
+                });
             }
             
-          if (!["revenue","expense"].includes(type)){
-            return res.status(400).json({
-                message: "Check if the type is gain or expense."
-            });
-          }
 
-          const register = await Registration.create({category, type, description, value, userId})
+          const register = await Registration.create({
+            category,
+            type,
+            description,
+            value,
+            userId: req.user.id,
+            })
 
         return res.status(201).json(register);
     } catch(error){
@@ -32,6 +41,70 @@ const registerController = {
             message: "Error in registration",
         });
     }
+    },
+
+    async updateRegister(req, res){
+        try{
+            const {id} = req.params;
+            const {category, type, description, value} = req.body;
+
+            const register = await Registration.findByPk(id);
+
+            if(!register){
+                return res.status(404).json({
+                    message: "Register not found"
+                })
+            }
+
+            if(value !== undefined && value < 0){
+                return res.status(400).json({
+                    message: "The value cannot be negative."
+                });
+                }
+            
+            
+            if (!["revenue","expense"].includes(type)){
+                return res.status(400).json({
+                    message: "Check if the type is revenue or expense."
+                });
+            }
+
+            await register.update({
+                category,
+                type,
+                description,
+                value,               
+            });
+
+            return res.json(register);
+        } catch (error){
+            console.error(error);
+            return res.status(500).json({
+                message: "Error in update register",
+        })
+    }},
+
+    async deleteRegister(req, res){
+        try{
+            const {id} = req.params;
+
+            const register = await Registration.findByPk(id);
+            if(!id){
+                return res.status(400).json({
+                    message: "register not found"
+                })
+            }
+
+            await register.destroy()
+
+            return res.json(register);
+
+        }catch(error){
+            console.error(error);
+            return res.status(500).json({
+                message: "Error in delete register"
+            })
+        }
     }
 
 
--- src/routes/register.router.js
+++ src/routes/register.router.js
@@ -4,6 +4,8 @@ import authMiddleware from "../middlewares/auth.middlewares.js";
 
 const registerRouter = express.Router();
 
-registerRouter.post("/registrations", authMiddleware, registerController.createRegister);
+registerRouter.post("/registrations/create", authMiddleware, registerController.createRegister);
+registerRouter.put("/registrations/update/:id", authMiddleware, registerController.updateRegister);
+registerRouter.delete("/registrations/delete/:id", authMiddleware, registerController.deleteRegister);
 
 export default registerRouter;
